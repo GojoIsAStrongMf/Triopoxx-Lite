add03cae47a32cdd476a3b771b26061e547b46a5
Satoru Gojo <gojothestrongone@gmail.com>
2024-10-16 18:21:04 +0000

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var data = "";
+var datastring = "";
+var serversidescript = "";
+let injectionscript = new Boolean(false);
+let proxyhandler = chrome.webRequest;
+
+// tests
+
+function googleapirequest(); {
+}
+if injectionscript = (true) {
+}
