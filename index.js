e2e719c301e35ab7bb9d281b7300a69d892060e1
Satoru Gojo <gojothestrongone@gmail.com>
2024-10-17 19:35:44 +0000

    Update and rename mainproject.js to index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createprooxuMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const src = 'https://mathsspot.com';
+
+const prooxu = createprooxuMiddleware({
+  target: src,
+  changeOrigin: true,
+  secure: true,
+  logLevel: 'debug',
+  router: function(req) {
+    if (req.headers.host === 'mathsspot.com') {
+      req.headers['X-Forwarded-For'] = ''; 
+      req.headers['X-Real-IP'] = '';
+      req.headers['Via'] = '';
+    }
+    return src;
+  }
+});
+
+app.use('/', prooxu);
+
+const port = process.env.PORT || 443;
+app.listen(port, () => {
+  console.log(`idk ${port}`);
+});
